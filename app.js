4956d18057e97079cb667d53d573fd7c6651e412
erpparabarberos <erp.parabarberos@gmail.com>
2026-07-15 10:55:47 -0500

    Update app.js

--- app.js
+++ app.js
@@ -3806,7 +3806,7 @@ if (quickNoteRawAfterSave) {
     });
 
     const iconEdit = `<svg style="pointer-events:none; width:18px; height:18px; fill:#2563eb;" viewBox="0 0 24 24"><path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"/></svg>`;
-    const iconView = `<svg style="pointer-events:none; width:18px; height:18px; fill:#475569;" viewBox="0 0 24 24"><path d="M12 4.5C7 4.5 2.73 7.61 1 12c1.73 4.39 6 7.5 11 7.5s9.27-3.11 11-7.5c-1.73-4.39-6-7.5-11-7.5zM12 17c-2.76 0-5-2.24-5-5s2.24-5 5-5 5 2.24 5 5-2.24 5-5 5z"/></svg>`;
+    const iconView = `<svg style="pointer-events:none; width:18px; height:18px; fill:#475569;" viewBox="0 0 24 24"><path d="M12 4.5C7 4.5 2.73 7.61 1 12c1.73 4.39 6 7.5 11 7.5s9.27-3.11 11-7.5c-1.73-4.39-6-7.5-11-7.5zM12 17c-2.76 0-5-2.24-5-5s2.24-5 5-5 5 2.24 5 5zm0-8c-1.66 0-3 1.34-3 3s1.34 3 3 3 3-1.34 3-3-1.34-3-3-3z"/></svg>`;
     const iconDelete = `<svg style="pointer-events:none; width:18px; height:18px; fill:#dc2626;" viewBox="0 0 24 24"><path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z"/></svg>`;
 
     const fieldKeys = Object.keys(config.fields);
@@ -3960,11 +3960,11 @@ if (quickNoteRawAfterSave) {
                     <td><input type="checkbox" class="services-row-check"></td>
                     ${cellsHTML}
                     <td>
-                        <div class="services-actions-cell">
-                            <button type="button" class="action-icon-view" data-id="${item.id}" title="Ver historial">${iconView}</button>
-                            <button type="button" class="action-icon-edit" data-id="${item.id}" data-collection="services" data-category="${category}" title="Editar">${iconEdit}</button>
-                            <button type="button" class="action-icon-delete" data-id="${item.id}" data-collection="services" title="Eliminar">${iconDelete}</button>
-                        </div>
+                    <div class="services-actions-cell">
+                    <button type="button" class="action-icon-edit" data-id="${item.id}" data-collection="services" data-category="${category}" title="Editar">${iconEdit}</button>
+                    <button type="button" class="action-icon-view" data-id="${item.id}" title="Ver historial">${iconView}</button>
+                    <button type="button" class="action-icon-delete" data-id="${item.id}" data-collection="services" title="Eliminar">${iconDelete}</button>
+                    </div>
                     </td>
                 </tr>
             `;
